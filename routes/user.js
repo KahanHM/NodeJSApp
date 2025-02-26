fd57a538b4b9d14962848a3049ab1d80a9455489
KahanHM <kahanahamed42@gmail.com>
2025-02-26 17:21:50 +0530

    Revert "adding unit test"
    This reverts commit ba9937af2aaf18d8f37082e0a7b5fcbdeb2975f7.

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const User = require('../models/user');
+
 const router = express.Router();
 
+// Create a new user
 router.post('/', async (req, res) => {
   try {
     const user = new User(req.body);
@@ -12,10 +14,50 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Get all users
 router.get('/', async (req, res) => {
   try {
     const users = await User.find();
-    res.send(users);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get a user by id
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update a user by id
+router.patch('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a user by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
   } catch (error) {
     res.status(500).send(error);
   }
